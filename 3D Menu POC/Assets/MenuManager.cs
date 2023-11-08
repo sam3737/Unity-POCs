604a5b012285032f574fced63c84ec86c5e6cfb4
Sam McDowell <sam.mcd.4321@gmail.com>
2023-11-08 10:35:32 -0700

    Add abilities

--- 3D Menu POC/Assets/MenuManager.cs
+++ 3D Menu POC/Assets/MenuManager.cs
@@ -5,16 +5,29 @@ using System;
 
 public class MenuManager : MonoBehaviour
 {
-    private Vector3 defaultCameraPos;
+
+    public static bool enabled = true;
+    public static Vector3 defaultCameraRotation;
+    public static Vector3 defaultCameraPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        defaultCameraPos = Camera.main.transform.eulerAngles;
+        defaultCameraRotation = Camera.main.transform.eulerAngles;
+        defaultCameraPosition = Camera.main.transform.position;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Camera.main.transform.position == defaultCameraPosition)
+        {
+            Camera.main.transform.eulerAngles = getTargetRotation();
+        }
+        
+    }
+
+    public static Vector3 getTargetRotation()
     {
         // Get the mouse position
         float freedom = 6;
@@ -22,6 +35,6 @@ public class MenuManager : MonoBehaviour
         float midX = maxX / 2;
         float mouseX = Mathf.Max(Mathf.Min(Input.mousePosition.x, maxX), 0);
         float rotation = freedom * Mathf.Sign(mouseX - midX) * Mathf.Pow(Mathf.Abs(((mouseX - midX) / midX)), 2);
-        Camera.main.transform.eulerAngles = defaultCameraPos + new Vector3(0f, rotation, 0f);
+        return defaultCameraRotation + new Vector3(0f, rotation, 0f);
     }
 }
--- 3D Menu POC/Assets/MenuOption.cs
+++ 3D Menu POC/Assets/MenuOption.cs
@@ -5,30 +5,107 @@ using UnityEngine;
 public class MenuOption : MonoBehaviour
 {
     public string optionName;
+    public bool subMenu;
+    public Vector3 cameraPos;
+    public Vector3 cameraRotation;
+    public float moveSpeed;
+    public float rotationSpeed;
+    private bool inSubmenu;
 
     public AudioClip myAudioClip;
     private AudioSource audioSource;
+
+    private Coroutine moveCoroutine;
+    private Coroutine rotationCoroutine;
 
     private void Awake()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.playOnAwake = false;
         audioSource.clip = myAudioClip;
+
+        inSubmenu = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) // Detect right click 
+        {
+            if (moveCoroutine != null)
+                StopCoroutine(moveCoroutine);
+            if (rotationCoroutine != null)
+                StopCoroutine(rotationCoroutine);
+
+            moveCoroutine = StartCoroutine(moveCameraPos(MenuManager.defaultCameraPosition));
+            rotationCoroutine = StartCoroutine(moveCameraRotBack());
+        }
     }
 
     void OnMouseDown()
     {
         Debug.Log(optionName);
+        if (subMenu)
+        {
+            if (moveCoroutine != null)
+                StopCoroutine(moveCoroutine);
+            if (rotationCoroutine != null)
+                StopCoroutine(rotationCoroutine);
+
+            moveCoroutine = StartCoroutine(moveCameraPos(cameraPos));
+            rotationCoroutine = StartCoroutine(moveCameraRot(cameraRotation));
+            MenuManager.enabled = false;
+        }
     }
 
     void OnMouseEnter()
     {
         audioSource.Play();
-        transform.Rotate(new Vector3(0, 10, 0));
+        if (!inSubmenu)
+            transform.Rotate(new Vector3(0, 10, 0));
     }
 
     void OnMouseExit()
     {
-        transform.Rotate(new Vector3(0, -10, 0));
+        if (!inSubmenu)
+            transform.Rotate(new Vector3(0, -10, 0));
+    }
+
+    // Start a subroutine to smoothly move the camera to the target position
+    IEnumerator moveCameraPos(Vector3 target)
+    {
+        while (Vector3.Distance(Camera.main.transform.position, target) > 0.001f)
+        {
+            float step = moveSpeed * Time.deltaTime;
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, target, step);
+            yield return null;
+        }
+
+        Camera.main.transform.position = target;
+    }
+
+    // Start a subroutine to smoothly move the camera to the target rotation
+    IEnumerator moveCameraRot(Vector3 target)
+    {
+        while (Quaternion.Angle(Camera.main.transform.rotation, Quaternion.Euler(target)) > 0.001f)
+        {
+            float step = rotationSpeed * Time.deltaTime;
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, Quaternion.Euler(target), step);
+            yield return null;
+        }
+
+        Camera.main.transform.rotation = Quaternion.Euler(target);
+    }
+
+    // Start a subroutine to smoothly move the camera to the target rotation
+    IEnumerator moveCameraRotBack()
+    {
+        while (Quaternion.Angle(Camera.main.transform.rotation, Quaternion.Euler(MenuManager.getTargetRotation())) > 0.001f)
+        {
+            float step = rotationSpeed * Time.deltaTime;
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, Quaternion.Euler(MenuManager.getTargetRotation()), step);
+            yield return null;
+        }
+
+        Camera.main.transform.rotation = Quaternion.Euler(MenuManager.getTargetRotation());
     }
 }
